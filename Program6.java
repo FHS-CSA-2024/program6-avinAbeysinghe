4b4b01ed2cc348315a779addcb56e53f24e99b86
Abeysinghe_A <151038579+avinAbeysinghe@users.noreply.github.com>
2024-08-16 14:10:47 -0700

    Update Program6.java
    Completed Assignment while setting the values to include up to 3 decimal places.

--- Program6.java
+++ Program6.java
@@ -1,6 +1,10 @@
 import java.util.Scanner;
 
 public class Program6 {
+    public static String convert(double value){
+        String stringValue = String.format("%.3f", value);     
+        return stringValue;
+    }
     public static void main(String[] args){
         final double pi = 3.14159;
         Scanner read = new Scanner(System.in);
@@ -12,10 +16,10 @@ public class Program6 {
         double diameter = radius * 2;
         double area = Math.pow(radius, 2) * pi;
         double circumferance = diameter * pi;
-        System.out.println("The Radius of the circle: " + radius);
-        System.out.println("The Diameter of the circle: " + diameter);
-        System.out.println("The Area of the circle: " + area);
-        System.out.println("The Circumferance of the circle: " + circumferance);
+        System.out.println("The Radius of the circle: " + convert(radius));
+        System.out.println("The Diameter of the circle: " + convert(diameter));
+        System.out.println("The Area of the circle: " + convert(area));
+        System.out.println("The Circumferance of the circle: " + convert(circumferance));
 
     }
 }
@@ -28,7 +32,7 @@ Enter the radius:
 3.712
 The Radius of the circle: 3.712
 The Diameter of the circle: 7.424
-The Area of the circle: 43.28779268096
-The Circumferance of the circle: 23.32316416
+The Area of the circle: 43.288
+The Circumferance of the circle: 23.323
 
 */
